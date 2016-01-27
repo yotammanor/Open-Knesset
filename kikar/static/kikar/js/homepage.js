022b1dd77abce8c05cf9ea7e63f17ce8baf3e6e6
Yotam <yotammanor@gmail.com>
2016-01-27 13:30:51 +0200

    fixed issue hasadna/open-knesset#494: loading statuses from kikar's live server, added loading symbol

--- kikar/static/kikar/js/homepage.js
+++ kikar/static/kikar/js/homepage.js
@@ -1,26 +1,31 @@
-function requestAndAddStatuses(url) {
-    var contentMaxLength = 200;
-    $.ajax({
-        url: url, contentType: "application/json", success: function (data) {
-            var listContainer = $('#kikar-facebook-updates-ul');
-            var nextBatchOfStatuses = $('#statuses-more');
-            nextBatchOfStatuses.data('next', 'http://localhost:8000' + data.meta.next);
-
-            data.objects.forEach(function (element, index, array) {
-                //console.log(element);
-                var source = $('#facebook-status-template').html();
-                var template = Handlebars.compile(source);
-                var testElem = {member: "test", party: "test2"};
-                var html = template(element);
-                //var htmlElem = $.parseHTML("<li class='agenda-mini clearfix'>" + element.content.substring(0, contentMaxLength) + "</li>");
-                listContainer.append(html);
-            });
-        }
-    });
-}
-
 $(document).ready(function () {
-    var url = 'http://localhost:8000/api/v1/facebook_status/?limit=5';
+    var kikarUrlBase = 'http://www.kikar.org';
+    var loadingSymbol = $('#loading-statuses-symbol');
+
+    function requestAndAddStatuses(url) {
+        var requestURL = '/kikar/get-statuses/?path=' + encodeURI(url);
+        console.log(requestURL);
+        loadingSymbol.show();
+        $.ajax({
+            url: requestURL, contentType: "application/json", success: function (data) {
+                var listContainer = $('#kikar-facebook-updates-ul');
+                var nextBatchOfStatuses = $('#statuses-more');
+                nextBatchOfStatuses.data('next', kikarUrlBase + data.meta.next);
+                loadingSymbol.hide();
+                data.objects.forEach(function (element, index, array) {
+                    //console.log(element);
+                    var source = $('#facebook-status-template').html();
+                    var template = Handlebars.compile(source);
+                    var testElem = {member: "test", party: "test2"};
+                    var html = template(element);
+                    //var htmlElem = $.parseHTML("<li class='agenda-mini clearfix'>" + element.content.substring(0, contentMaxLength) + "</li>");
+                    listContainer.append(html);
+                });
+            }
+        });
+    }
+
+    var url = kikarUrlBase + '/api/v1/facebook_status/?limit=5';
     requestAndAddStatuses(url);
 
     $('#statuses-more').on("click", function (event) {
