0a1c6efdf0074b2a1cb7baa1847f86c3587a25be
Yotam <yotammanor@gmail.com>
2016-01-27 13:54:40 +0200

    fixed issue hasadna/open-knesset#494: added basic error handling, and refactored number of statuses per request to a separate constant.

--- kikar/static/kikar/js/homepage.js
+++ kikar/static/kikar/js/homepage.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
     var kikarUrlBase = 'http://www.kikar.org';
     var loadingSymbol = $('#loading-statuses-symbol');
-
+    var statusNumLimitPerRequest = 5;
     function requestAndAddStatuses(url) {
         var requestURL = '/kikar/get-statuses/?path=' + encodeURI(url);
         console.log(requestURL);
         loadingSymbol.show();
+        var listContainer = $('#kikar-facebook-updates-ul');
         $.ajax({
-            url: requestURL, contentType: "application/json", success: function (data) {
-                var listContainer = $('#kikar-facebook-updates-ul');
+            url: requestURL, contentType: "application/json",
+            success: function (data) {
                 var nextBatchOfStatuses = $('#statuses-more');
                 nextBatchOfStatuses.data('next', kikarUrlBase + data.meta.next);
                 loadingSymbol.hide();
@@ -21,11 +22,15 @@ $(document).ready(function () {
                     //var htmlElem = $.parseHTML("<li class='agenda-mini clearfix'>" + element.content.substring(0, contentMaxLength) + "</li>");
                     listContainer.append(html);
                 });
+            },
+            error: function () {
+                loadingSymbol.hide();
+                listContainer.append("<p>התנצלותינו הכנה, ישנה תקלה בקטעינת הסטאטוס.</p>")
             }
         });
     }
 
-    var url = kikarUrlBase + '/api/v1/facebook_status/?limit=5';
+    var url = kikarUrlBase + '/api/v1/facebook_status/?limit=' + statusNumLimitPerRequest;
     requestAndAddStatuses(url);
 
     $('#statuses-more').on("click", function (event) {
