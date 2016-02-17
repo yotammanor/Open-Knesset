ffbbfd766225d881e9f91cefc0f68ea60c854965
alonisser <alonisser@gmail.com>
2016-02-17 10:28:29 +0200

    add filters for missing laws at bills admin

--- laws/admin.py
+++ laws/admin.py
@@ -1,5 +1,6 @@
 from models import Vote, Law, PrivateProposal, KnessetProposal, GovProposal, Bill, GovLegislationCommitteeDecision
 from laws.management.commands.scrape_votes import Command as ScrapeVotesCommand
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 
 
@@ -60,8 +61,45 @@ class GovProposalAdmin(admin.ModelAdmin):
 admin.site.register(GovProposal, GovProposalAdmin)
 
 
+class MissingLawListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('Missing Laws')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'is_missing_law'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('missing_law', _('Has Missing Law')),
+            # ('90s', _('in the nineties')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == 'missing_law':
+            return queryset.filter(law=None)
+        else:
+            return queryset
+
+
 class BillAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('law', 'title', 'stage')
+    search_fields = ('title',)
+    list_filter = ('stage', MissingLawListFilter)
 
 
 admin.site.register(Bill, BillAdmin)
