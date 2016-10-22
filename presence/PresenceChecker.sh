e30256628e96d6891f4ee072cb8af4508b4d6029
alonisser <alonisser@gmail.com>
2016-10-22 17:19:02 +0300

    fix path to presence.txt in bash script

--- presence/PresenceChecker.sh
+++ presence/PresenceChecker.sh
@@ -3,6 +3,6 @@ if [ `pgrep -f PresenceManager.py` ]; then
     kill `ps -ef | grep -v grep | grep "PresenceManager" | head -1 | awk '{print $2}'`
     sleep 1s
 fi
-cat presence.txt | grep $(date +"%Y") | gzip  > /oknesset_data/oknesset/Open-Knesset/data/presence.txt.gz
+cat /oknesset_data/presence/presence.txt | grep $(date +"%Y") | gzip  > /oknesset_data/oknesset/Open-Knesset/data/presence.txt.gz
 cd /oknesset_data/presence
 python PresenceManager.py &
