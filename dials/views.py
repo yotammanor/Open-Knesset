295a37a91a1e0429da07326ae43ff7b893f19bbb
Benny Daon <bennydaon@gmail.com>
2016-12-13 09:50:53 +0200

    finishing up the dial - #759
    - better urls
    - better template file names
    - make the svg template the main template
    - add embedding instruction
    - add styling to the descrription page

--- dials/views.py
+++ dials/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
 from models import Dial
 
 # Create your views here.
@@ -11,15 +12,18 @@ def dial_svg(request, slug):
         color = "orange" #TODO: pick a color
     else:
         color = "green" #TODO: pick a color
-    return render_to_response("dials/svg.html",
+    return render_to_response("dials/dial.html",
             {'width': dial.precent * 746 / 100,
              'precent': dial.precent,
              'color': color,
-             })
+             'slug': dial.slug,
+             },
+            context_instance=RequestContext(request))
 
 
-def dial_html(request, slug):
+def dial_desc(request, slug):
     dial = get_object_or_404(Dial, slug=slug)
-    return render_to_response("dials/text.html",
-            {'dial': dial})
+    return render_to_response("dials/desc.html",
+            {'dial': dial},
+            context_instance=RequestContext(request))
 
