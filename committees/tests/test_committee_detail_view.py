df2742032869090d9da516b140991d2f56b60a6b
Yotam <yotammanor@gmail.com>
2016-12-15 16:56:27 +0200

    add committee methods to access members:
    - add a generic list of all members of a committee (chairpersons and replacements included)
    - add a method that returns the list ordered by name.
    - refactor members_by_presence to use generic method.
    - add tests to Member methods.

--- committees/tests/test_committee_detail_view.py
+++ committees/tests/test_committee_detail_view.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+
 from django.test import TestCase
 from django.conf import settings
 from django.core.urlresolvers import reverse
@@ -6,6 +7,7 @@ from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
 from tagging.models import Tag, TaggedItem
+from waffle import testutils as waffle_testutils
 
 from committees.tests.base import BaseCommitteeTestCase
 from laws.models import Bill
@@ -20,7 +22,8 @@ class CommitteeDetailViewTest(BaseCommitteeTestCase):
     def setUp(self):
         super(CommitteeDetailViewTest, self).setUp()
         self.knesset = Knesset.objects.create(number=1,
-                                              start_date=datetime.today() - timedelta(days=1))
+                                              start_date=datetime.today() - timedelta(
+                                                  days=1))
         self.committee_1 = Committee.objects.create(name='c1')
         self.committee_2 = Committee.objects.create(name='c2')
         self.meeting_1 = self.committee_1.meetings.create(date=datetime.now(),
@@ -41,16 +44,19 @@ I have a deadline''')
         (self.group, created) = Group.objects.get_or_create(name='Valid Email')
         if created:
             self.group.save()
-        self.group.permissions.add(Permission.objects.get(name='Can add annotation'))
+        self.group.permissions.add(
+            Permission.objects.get(name='Can add annotation'))
         self.jacob.groups.add(self.group)
 
         ct = ContentType.objects.get_for_model(Tag)
-        self.adrian.user_permissions.add(Permission.objects.get(codename='add_tag', content_type=ct))
+        self.adrian.user_permissions.add(
+            Permission.objects.get(codename='add_tag', content_type=ct))
 
         self.bill_1 = Bill.objects.create(stage='1', title='bill 1')
         self.mk_1 = Member.objects.create(name='mk 1')
         self.topic = self.committee_1.topic_set.create(creator=self.jacob,
-                                                       title="hello", description="hello world")
+                                                       title="hello",
+                                                       description="hello world")
         self.tag_1 = Tag.objects.create(name='tag1')
         self.meeting_1.mks_attended.add(self.mk_1)
 
@@ -87,8 +93,10 @@ I have a deadline''')
                          [self.meeting_1.id, self.meeting_2.id, ],
                          'object_list has wrong objects: %s' % object_list)
 
-    def test_committee_does_not_return_non_active_current_knesset_members(self):
-        non_current_mk = Member.objects.create(name='mk is no more', is_current=False)
+    def test_committee_does_not_return_non_active_current_knesset_members(
+            self):
+        non_current_mk = Member.objects.create(name='mk is no more',
+                                               is_current=False)
         self.given_mk_is_added_to_committee(self.committee_1, non_current_mk)
         self.given_mk_is_added_to_committee(self.committee_1, self.mk_1)
 
@@ -98,10 +106,13 @@ I have a deadline''')
         self.verify_expected_members_in_context(res, [self.mk_1.pk])
         self.verify_unexpected_members_not_in_context(res, [non_current_mk.pk])
 
-    def test_committee_does_not_return_non_active_current_knesset_members_who_were_replacements(self):
-        non_current_mk = Member.objects.create(name='mk is no more', is_current=False)
+    def test_committee_does_not_return_non_active_current_knesset_members_who_were_replacements(
+            self):
+        non_current_mk = Member.objects.create(name='mk is no more',
+                                               is_current=False)
         self.given_mk_is_added_to_committee(self.committee_1, non_current_mk)
-        self.given_mk_is_added_to_committee_as_replacment(self.committee_1, non_current_mk)
+        self.given_mk_is_added_to_committee_as_replacment(self.committee_1,
+                                                          non_current_mk)
         self.given_mk_is_added_to_committee(self.committee_1, self.mk_1)
 
         res = self.client.get(self.committee_1.get_absolute_url())
@@ -110,10 +121,13 @@ I have a deadline''')
         self.verify_expected_members_in_context(res, [self.mk_1.pk])
         self.verify_unexpected_members_not_in_context(res, [non_current_mk.pk])
 
-    def test_committee_does_not_return_non_active_current_knesset_members_who_were_person(self):
-        non_current_mk = Member.objects.create(name='mk is no more', is_current=False)
+    def test_committee_does_not_return_non_active_current_knesset_members_who_were_person(
+            self):
+        non_current_mk = Member.objects.create(name='mk is no more',
+                                               is_current=False)
         self.given_mk_is_added_to_committee(self.committee_1, non_current_mk)
-        self.given_mk_is_added_to_committee_as_chairperson(self.committee_1, non_current_mk)
+        self.given_mk_is_added_to_committee_as_chairperson(self.committee_1,
+                                                           non_current_mk)
         self.given_mk_is_added_to_committee(self.committee_1, self.mk_1)
 
         res = self.client.get(self.committee_1.get_absolute_url())
@@ -123,7 +137,8 @@ I have a deadline''')
         self.verify_unexpected_members_not_in_context(res, [non_current_mk.pk])
 
     def test_committeemeeting_by_tag(self):
-        res = self.client.get('%s?tagged=false' % reverse('committee-all-meetings'))
+        res = self.client.get(
+            '%s?tagged=false' % reverse('committee-all-meetings'))
         self.assertQuerysetEqual(res.context['object_list'],
                                  ['<CommitteeMeeting: c1 - python>',
                                   '<CommitteeMeeting: c1 - django>'],
@@ -132,15 +147,112 @@ I have a deadline''')
             tag=self.tag_1,
             content_type=ContentType.objects.get_for_model(CommitteeMeeting),
             object_id=self.meeting_1.id)
-        res = self.client.get(reverse('committeemeeting-tag', args=[self.tag_1.name]))
+        res = self.client.get(
+            reverse('committeemeeting-tag', args=[self.tag_1.name]))
         self.assertEqual(res.status_code, 200)
-        self.assertTemplateUsed(res, 'committees/committeemeeting_list_by_tag.html')
+        self.assertTemplateUsed(res,
+                                'committees/committeemeeting_list_by_tag.html')
         tag = res.context['tag']
         self.assertEqual(tag, self.tag_1)
         self.assertQuerysetEqual(res.context['object_list'],
                                  ['<CommitteeMeeting: c1 - django>'])
-        res = self.client.get('%s?tagged=false' % reverse('committee-all-meetings'))
+        res = self.client.get(
+            '%s?tagged=false' % reverse('committee-all-meetings'))
         self.assertQuerysetEqual(res.context['object_list'],
                                  ['<CommitteeMeeting: c1 - python>'])
         # cleanup
         self.ti.delete()
+
+    def test_committee_all_members_return_all_types_of_related_members(self):
+        committee_3 = Committee.objects.create(name='c3')
+        chairperson_mk = Member.objects.create(name='Chief')
+        member_mk = Member.objects.create(name='member')
+        replacement_mk = Member.objects.create(name='replacement')
+        committee_3.members.add(member_mk)
+        committee_3.chairpersons.add(chairperson_mk)
+        committee_3.replacements.add(replacement_mk)
+
+        all_members = committee_3.all_members()
+        self.assertIn(member_mk, all_members)
+        self.assertIn(replacement_mk, all_members)
+        self.assertIn(chairperson_mk, all_members)
+
+        # cleanup
+        committee_3.members.all().delete()
+        committee_3.chairpersons.all().delete()
+        committee_3.replacements.all().delete()
+        chairperson_mk.delete()
+        member_mk.delete()
+        replacement_mk.delete()
+        committee_3.delete()
+
+    def test_committee_members_by_name_returns_in_correct_order(self):
+        committee_3 = Committee.objects.create(name='c3')
+        member_1 = Member.objects.create(name='member_1')
+        member_3 = Member.objects.create(name='member_3')
+        member_2 = Member.objects.create(name='member_2')
+        committee_3.members.add(member_1)
+        committee_3.members.add(member_3)
+        committee_3.members.add(member_2)
+
+        members_by_name = committee_3.members_by_name()
+        self.assertEqual(member_1, members_by_name[0])
+        self.assertEqual(member_3, members_by_name[-1])
+
+        # cleanup
+        committee_3.members.all().delete()
+        member_1.delete()
+        member_2.delete()
+        member_3.delete()
+        committee_3.delete()
+
+    def test_committee_members_by_presence_returns_in_correct_order(self):
+        committee_3 = Committee.objects.create(name='c3')
+        meeting_1 = committee_3.meetings.create(date=datetime.now(),
+                                                topics="django",
+                                                protocol_text='''jacob:
+        I am a perfectionist
+        adrian:
+        I have a deadline''')
+        meeting_2 = committee_3.meetings.create(date=datetime.now(),
+                                                topics="python",
+                                                protocol_text='''spam spam''')
+        member_1 = Member.objects.create(name='member_1')
+        member_3 = Member.objects.create(name='member_3')
+        member_2 = Member.objects.create(name='member_2')
+        committee_3.members.add(member_1)
+        committee_3.members.add(member_3)
+        committee_3.members.add(member_2)
+        meeting_1.mks_attended.add(member_3)
+        meeting_2.mks_attended.add(member_3)
+        meeting_2.mks_attended.add(member_1)
+
+        members_by_presence = committee_3.members_by_presence()
+        self.assertEqual([member_3, member_1, member_2], members_by_presence)
+
+        # cleanup
+        committee_3.members.all().delete()
+        member_1.delete()
+        member_2.delete()
+        member_3.delete()
+        meeting_1.delete()
+        meeting_2.delete()
+        committee_3.delete()
+
+    @waffle_testutils.override_flag('show_member_presence', active=True)
+    def test_committee_has_show_presence_in_context_when_flag_is_set(
+            self):
+        res = self.client.get(self.committee_1.get_absolute_url())
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(res.context['show_member_presence'],
+                        msg="context property 'show_member_presence'"
+                            " was not set to True.")
+
+    @waffle_testutils.override_flag('show_member_presence', active=False)
+    def test_committee_does_not_show_presence_in_context_when_flag_is_unset(
+            self):
+        res = self.client.get(self.committee_1.get_absolute_url())
+        self.assertEqual(res.status_code, 200)
+        self.assertFalse(res.context['show_member_presence'],
+                         msg="context property 'show_member_presence'"
+                             " was not set to False.")
