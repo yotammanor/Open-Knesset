bf87dd4cd700cb464d5c1037f8023cbfb0e879a7
alonisser <alonisser@gmail.com>
2017-01-14 21:57:35 +0200

    Remove unneeded view

--- simple/urls.py
+++ simple/urls.py
@@ -1,9 +1,1 @@
 from django.conf.urls import url, patterns
-
-
-urlpatterns = patterns('',
-    #(r'^$', 'canvas'),
-    url(r'^hello/$', 'knesset.simple.views.hello_world'),
-
-    # Define other pages you want to create here
-)
